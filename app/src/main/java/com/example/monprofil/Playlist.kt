2ffd6ba6e4a7013911bbcca549ca10ffa9be0178
NDL4551A <elias.nodon@gmail.com>
2024-12-03 14:39:31 +0100

    modele ok

--- app/src/main/java/com/example/monprofil/Playlist.kt
+++ app/src/main/java/com/example/monprofil/Playlist.kt
@@ -1,45 +1,75 @@
 package com.example.monprofil
 
+data class Playlist(
+    val checksum: String,
+    val collaborative: Boolean,
+    val cover: String,
+    val creation_date: String,
+    val creator: Creator,
+    val description: String,
+    val duration: Int,
+    val fans: Int,
+    val id: Int,
+    val is_loved_track: Boolean,
+    val link: String,
+    val md5_image: String,
+    val nb_tracks: Int,
+    val picture_type: String,
+    val `public`: Boolean,
+    val share: String,
+    val title: String,
+    val tracklist: String,
+    val tracks: Tracks,
+    val type: String
+)
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import coil.compose.rememberImagePainter
+data class Creator(
+    val id: Int,
+    val name: String,
+    val tracklist: String,
+    val type: String
+)
 
+data class Tracks(
+    val checksum: String,
+    val `data`: List<Data>
+)
 
-@Composable
-fun PlaylistScreen(viewModel: MainViewModel = viewModel(), searchQuery: String, navController: NavController) {
+data class Data(
+    val album: Album,
+    val artist: Artist,
+    val duration: Int,
+    val explicit_content_cover: Int,
+    val explicit_content_lyrics: Int,
+    val explicit_lyrics: Boolean,
+    val id: Long,
+    val isrc: String,
+    val link: String,
+    val md5_image: String,
+    val preview: String,
+    val rank: Int,
+    val readable: Boolean,
+    val time_add: Int,
+    val title: String,
+    val title_short: String,
+    val title_version: String,
+    val type: String
+)
 
+data class Album(
+    val cover: String,
+    val id: Int,
+    val md5_image: String,
+    val title: String,
+    val tracklist: String,
+    val type: String,
+    val upc: String
+)
 
-
-}
-
+data class Artist(
+    val id: Int,
+    val link: String,
+    val name: String,
+    val tracklist: String,
+    val type: String
+)
---
+++ app/src/main/java/com/example/monprofil/PlaylistScreen.kt
@@ -0,0 +1,15 @@
+package com.example.monprofil
+
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+
+@Composable
+fun PlaylistScreen(viewModel: MainViewModel = viewModel(), searchQuery: String, navController: NavController) {
+
+
+
+}
+
