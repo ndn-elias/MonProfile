74e313e28c10ec1fc2679780a650a945303a2d0a
NDL4551A <elias.nodon@gmail.com>
2024-12-03 14:26:21 +0100

    exercice 2 ok

---
+++ app/src/main/java/com/example/monprofil/Etudiant.kt
@@ -0,0 +1,9 @@
+package com.example.monprofil
+
+class Etudiant(val name: String, val promo: String, val matieres: List<String>) {
+
+
+
+
+
+}
--- app/src/main/java/com/example/monprofil/Main.kt
+++ app/src/main/java/com/example/monprofil/Main.kt
@@ -14,4 +14,27 @@ fun main(){
         println(item.surface())
 
     }
+    val etudiants = listOf(
+        Etudiant("Paul", "2025", listOf("mobile", "web", "reseau")),
+        Etudiant("Yazid", "2024", listOf("mobile", "android", "reseau")),
+        Etudiant("Caroline", "2025", listOf("Anglais", "SE"))
+    )
+
+
+    etudiants.filter { it.promo == "2024"}.forEach { println(it.name) }
+
+    for (etudiant in etudiants){
+        if (etudiant.matieres.count()>2){
+            println(etudiant.name)
+        }
+    }
+
+    var nbmatieres : Int = 0
+    for (etudiant in etudiants){
+        nbmatieres = nbmatieres + etudiant.matieres.count()
+        }
+    println(nbmatieres)
+
+
+
 }
